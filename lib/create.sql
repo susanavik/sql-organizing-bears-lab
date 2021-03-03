d63747f4dbee0290b2a897f8b6e8822fb046dbfd
Susana <susana.m.vik@gmail.com>
2021-03-03 18:01:41 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+create table bears(
+    id INTEGER primary key,
+    name text,
+    age integer,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+insert into bears (name, age, sex, color, temperament, alive) values 
+("Mr. Chocolate", 10, "M", "Brown", "sweet", 1);
+
+insert into bears (name, age, sex, color, temperament, alive) values
+("Rowdy", 4, "M", "White", "crazy", 1);  
+
+insert into bears (name, age, sex, color, temperament, alive) values
+("Tabitha", 30, "F", "Black", "Salty", 1);
+
+insert into bears (name, age, sex, color, temperament, alive) values
+("Sergeant Brown", 45, "M", "Brown", "mean", 0);
+
+insert into bears (name, age, sex, color, temperament, alive) values
+("Melissa", 24, "F", "Black", "calm", 0);
+
+insert into bears (name, age, sex, color, temperament, alive) values
+("Grinch", 65, "M", "Green", "grumpy", 0);
+
+insert into bears (name, age, sex, color, temperament, alive) values
+("Wendy", 45, "F", "White", "friendly", 1);
+
+insert into bears (name, age, sex, color, temperament, alive) values
+(null, 45, "M", "White", "angry", 1);
+
